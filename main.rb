7b6aa76c1501fb3da01e18833b6d3b988630d37d
Guven Karanfil <guvenkaranfil235@gmail.com>
2024-09-25 10:48:39 +0300

    export test result to related paths

--- main.rb
+++ main.rb
@@ -8,10 +8,7 @@ end
 
 $output_path = env_has_key("AC_OUTPUT_DIR") || abort("Missing AC_OUTPUT_DIR.")
 $repo_path = env_has_key("AC_REPOSITORY_DIR") || abort("Missing AC_REPOSITORY_DIR.")
-$npm_params = env_has_key("AC_RN_TEST_COMMAND_ARGS") || "jest"
-$test_result_path = "#{$output_path}/test.json"
-
-puts "REPO DIR #{env_has_key("AC_REPOSITORY_DIR")}"
+$npm_params = env_has_key("AC_RN_TEST_COMMAND_ARGS")
 
 def run_command(command)
     puts "@@[command] #{command}"
@@ -32,27 +29,25 @@ def run_command(command)
     end
 end
 
-puts "Repo Path #{$repo_path}"
-run_command("ls -la")
-
-$yarn_or_npm = "npm"
-if File.file?("#{$repo_path}/yarn.lock")
-    yarn_or_npm = "yarn"
-end
-
 def runTests
-    run_command("cd #{$repo_path} && ls -la") # add additional params from user
     yarn_or_npm = "npm"
     if File.file?("#{$repo_path}/yarn.lock")
         yarn_or_npm = "yarn"
     end
 
-    run_command("cd #{$repo_path} && yarn jest --reporters=default --reporters=jest-junit --coverage --coverageDirectory='coverage' --coverageReporters='json' --coverageReporters='lcov' && ls -la")
-
-    File.open(ENV['AC_ENV_FILE_PATH'], 'a') do |f|
-        f.puts "AC_TEST_RESULT_PATH=#{$output_path}/junit.xml"
-    end
+    report_command = $npm_params.nil? ? "jest --reporters=default --reporters=jest-junit --coverage --coverageDirectory='coverage' --coverageReporters='json' --coverageReporters='lcov'" : $npm_params
+    run_command("cd #{$repo_path} && #{yarn_or_npm} #{report_command}")
     
+    # copy test results to output directory for downloadable artifacts
+    run_command("cp #{$repo_path}/junit.xml #{$output_path}")
+    run_command("cp -r #{$repo_path}/coverage #{$output_path}")
+    
+
+    # Write AC_TEST_RESULT_PATH reserved variable to the ENV file for test report component
+    File.open(ENV['AC_ENV_FILE_PATH'], 'a') do |f|
+        f.puts "AC_TEST_RESULT_PATH=#{$output_path}"
+    end
+    # Write AC_COVERAGE_RESULT_PATH reserved variable(coverage path) to the ENV file  report for test report component
     File.open(ENV['AC_ENV_FILE_PATH'], 'a') do |f|
         f.puts "AC_COVERAGE_RESULT_PATH=#{$output_path}/coverage"
     end
